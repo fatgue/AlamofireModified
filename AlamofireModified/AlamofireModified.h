f665983475a215ba3dc4b7d6ff46748c01d8355c
Fatih Gürbüz <fguerbuez@smartwireless.de>
2021-09-17 10:20:35 +0200

    init

---
+++ AlamofireModified/AlamofireModified.h
@@ -0,0 +1,18 @@
+//
+//  AlamofireModified.h
+//  AlamofireModified
+//
+//  Created by Fatih Gürbüz on 17.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlamofireModified.
+FOUNDATION_EXPORT double AlamofireModifiedVersionNumber;
+
+//! Project version string for AlamofireModified.
+FOUNDATION_EXPORT const unsigned char AlamofireModifiedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireModified/PublicHeader.h>
+
+
